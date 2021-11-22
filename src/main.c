7bd0d4b1d868f851f44d4866350b3679d131b9c1
杨鑫锐 <2890819844@qq.com>
2021-11-22 08:28:21 +0800

    yangxr

--- src/main.c
+++ src/main.c
@@ -2,51 +2,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "except.h"
-#include "memchk.h"
+#include "list_head.h"
 
-static void test_mem();
-static void test_expect();
-
-int main()
+int main(int argc, const char *argv[])
 {
-	test_expect();
+	
 	return 0;
-}
-
-
-static void test_mem()
-{
-	char *ptr = NULL;
-	FILE *fp;
-
-	fp = fopen("./log", "a");
-	mem_log(fp);
-
-	assert("result " && 1);
-
-	ptr = malloc(100);
-	strcpy(ptr, "hello world\n");
-	printf("ptr : %s", ptr);
-	ptr = realloc(ptr, 1000);
-	ptr = calloc(11, 11);
-	mem_leak();
-
-	fclose(fp);
-}
-
-struct Except_T E1 = {.reason = "except e1 for test"};
-
-static void 
-test_expect_func()
-{
-	RAISE(E1);
-}
-
-static void 
-test_expect()
-{
-TRY
-	test_expect_func();
-END_TRY;
 }
