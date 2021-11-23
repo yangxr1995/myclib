8d5e5dcda8d36b9465e2481d1befe568d7155867
杨鑫锐 <2890819844@qq.com>
2021-11-23 11:34:34 +0800

    yangxr

---
+++ include/str.h
@@ -0,0 +1,19 @@
+#ifndef STR_H
+
+#define STR_H
+
+typedef struct str_s str_t;
+struct str_s {
+	int len;
+	char *data;
+};
+
+#define str_new(str) { sizeof(str) - 1, (char *	)str }
+#define str_null { 0, NULL }
+#define str_set_text(str, text) \
+	(str)->len = sizeof(text) - 1; (str)->data = text;
+#define str_set_null(str) \
+	(str)->len = 0; (str)->data = NULL
+
+
+#endif /* end of include guard: STR_H */
