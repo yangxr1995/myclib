8a022a76538f53ee3803b99b85a4a0cb4d5618e7
杨鑫锐 <2890819844@qq.com>
2021-11-23 18:06:13 +0800

    yangxr

--- include/str.h
+++ include/str.h
@@ -11,10 +11,15 @@ struct str_s {
 #define str_new(str) { sizeof(str) - 1, (char *	)str }
 #define str_null { 0, NULL }
 #define str_set_text(str, text) \
-	(str)->len = sizeof(text) - 1; (str)->data = text;
+	(str)->len = sizeof(text) - 1; (str)->data = text
 #define str_set_null(str) \
 	(str)->len = 0; (str)->data = NULL
 
 #define const_str_len(text) (sizeof(text) - sizeof((text)[0]))
+
+#define str_set_ptr(str, ptr) \
+	(str)->len = strlen(ptr); (str)->data = (char *)ptr
+#define str_set_comm(str, _data, _len) \
+	(str)->len = _len; (str)->data = (char *)_data
 
 #endif /* end of include guard: STR_H */
