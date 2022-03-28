2287dbe1f70a9a7d86138a62ce8d879c2da61665
杨鑫锐 <2890819844@qq.com>
2022-03-28 18:08:45 +0800

    yangxr

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 all:test
-test:src/memchk.c src/main.c src/assert.c src/except.c src/arena.c src/arena.c
-	gcc -I./include $^ -g -O0 -o $@
+test:src/memchk.c src/main.c src/assert.c
+	gcc -I./include $^ -g -O0 -o $@ -rdynamic
 
 clean:
 	rm -f test
--- src/main.c
+++ src/main.c
@@ -4,27 +4,22 @@
 #include <stdlib.h>
 
 #include "assert.h"
-#include "memchk.h"
-#include "fmt.h"
-#include "arena.h"
 
 const char *str_mem(void *ptr, size_t size);
 
+void func2()
+{
+
+	assert(0 && "bad bad");
+}
+
+void func()
+{
+	func2();
+}
 
 int main(int argc, const char *argv[])
 {
-	FILE *fp;
-	fp = fopen("./log", "a");
-	mem_log(fp);
-
-	Arena_T ar;
-	ar = Arena_new();
-	char *ptr = Arena_alloc(ar, 100, __FILE__, __LINE__);
-	memset(ptr, 0x0, 100);
-	Arena_free(ar);
-	Arena_dispose(&ar);
-
-	mem_leak();
-
+	func();
 	return 0;
 }
