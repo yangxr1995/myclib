b982738f84d117c6aac805f449c0776fd65ab700
yangxr <yangxr1995@gmail.com>
2023-12-13 16:55:41 +0800

    cmd

---
+++ src/cmd.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+typedef struct cmd_s cmd_t;
+
+struct cmd_s {
+	char *name;
+	char *help;
+};
+
+/*
+ * __attribute__((used)) : 
+ *    汇编时，将修饰符号标记到目标文件, 
+ *    确保链接时会合并符号，即使符号未被引用
+ *
+ * __attribute__((section("xxx"))) :
+ *    将修饰符号在指定段分配空间，若不指定，
+ *    则随机分配空间
+ *    段的起始地址为 &__start_xxx
+ *        结束地址为 &__stop_xxx
+ */
+
+cmd_t cmd_echo
+__attribute__((used)) 
+__attribute__((section("cmd"))) = {
+	.name = "echo",
+	.help = "echo <xxx>\n",
+};
+
+cmd_t cmd_cat
+__attribute__((used)) 
+__attribute__((section("cmd"))) = {
+	.name = "cat",
+	.help = "cat <xxx>\n",
+};
+
+extern cmd_t __start_cmd;
+extern cmd_t __stop_cmd;
+
+int main()
+{
+	cmd_t *cmd;	
+
+	for (cmd = &__start_cmd; cmd < &__stop_cmd;
+		cmd = (cmd_t *)((char *)cmd + sizeof(*cmd))) {
+		printf("name : %s, help : %s\n", cmd->name, cmd->help);	
+	}
+
+	return 0;
+}
