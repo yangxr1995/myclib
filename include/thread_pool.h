5392260f691bfc73eceda064047b36acb31c2afe
yangxr <yangxr1995@gmail.com>
2024-09-25 00:07:32 +0800

    async_work

--- include/thread_pool.h
+++ include/thread_pool.h
@@ -1,12 +1,10 @@
-#ifndef __THREAD_POOL_H
-#define __THREAD_POOL_H
-
+#pragma once
 
 typedef struct threadpool_s *threadpool_t;
+
 typedef void (*process_t) (void *);
 
 int threadpool_append(threadpool_t tp, process_t process, void *arg);
 void threadpool_delete(threadpool_t tp);
 threadpool_t threadpool_new(unsigned int tp_num, unsigned int max_requests, void (*init_worker)(void *), void *init_worker_arg);
 
-#endif
